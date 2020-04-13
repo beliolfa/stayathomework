94357cb3755f5cfc7ac4eaa99d254fac8f65c088
Xavi Sánchez Mir <xavi.sanchez.mir@gmail.com>
2020-04-13 10:22:38 +0200

    Adding i18n to the project (#8)
    Login and Index are already translated, more pages needs translation.

---
+++ config/i18n/index.js
@@ -0,0 +1,14 @@
+import en from './locales/en-US'
+import es from './locales/es-ES'
+
+export const I18N = {
+  locales: ['es', 'en'],
+  defaultLocale: 'es',
+  vueI18n: {
+    fallbackLocale: 'es',
+    messages: {
+      en,
+      es
+    }
+  }
+}
